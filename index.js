c5ac6aab9f32459d4a4ec23ef53aeef22bfee939
eishawasi <165848773+eishawasi@users.noreply.github.com>
2024-04-03 03:51:35 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env noden
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number ", type: "number", name: "first number" },
+    { message: "Enter second number", type: "number", name: "second number" },
+    {
+        message: "select one of the opration to perfom action",
+        type: "list",
+        name: "opreator",
+        choices: ["adition", "subtraction", "multiplication", "divide"],
+    },
+]);
+if (answer.opreator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
